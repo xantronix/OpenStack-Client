fadbaec7d01ff7a60eea35f5f4b54ef4e44044fa
Alexandra Hrefna Hilmisdóttir <xan@cpanel.net>
2015-10-29 19:18:18 +0000

    POD is so great, I decided to write it for OpenStack::Client, too (with a few small semantic improvements in code along the way)

--- lib/OpenStack/Client.pm
+++ lib/OpenStack/Client.pm
@@ -10,8 +10,27 @@ use LWP::UserAgent ();
 
 use OpenStack::Client::Base ();
 
+=head1 NAME
+
+OpenStack::Client - A reasonable OpenStack client
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 INSTANTIATION
+
+=over
+
+=item C<OpenStack::Client-E<gt>new(I<$endpoint>)>
+
+Create a new OpenStack client interface to the specified Keystone authentication
+and authorization I<$endpoint>.
+
+=cut
+
 sub new ($%) {
-    my ($class, $endpoint, %opts) = @_;
+    my ($class, $endpoint) = @_;
 
     die('No API authentication endpoint provided') unless $endpoint;
 
@@ -22,6 +41,144 @@ sub new ($%) {
         'auth'      => OpenStack::Client::Base->new($endpoint)
     }, $class;
 }
+
+=back
+
+=head1 AUTHORIZING WITH KEYSTONE
+
+=over
+
+=item C<$client-E<gt>auth(I<%args>)>
+
+Obtain an authorization token with the OpenStack Keystone service with the
+parameters specified in I<%args>.  The following arguments are required:
+
+=over
+
+=item * B<tenant>
+
+The OpenStack tenant (project) name
+
+=item * B<username>
+
+The OpenStack user name
+
+=item * B<password>
+
+The OpenStack password
+
+=back
+
+When successful, this method will return the Keystone authorization token found
+within the response body, and will allow the C<$client-E<gt>service()> method to
+access the endpoints the client has subsequently gained access to.
+
+After a successful call to this method, subsequent calls will simply return the
+existing authorization token data.
+
+=cut
+
+sub auth ($%) {
+    my ($self, %args) = @_;
+
+    die('No OpenStack tenant name provided in "tenant"') unless defined $args{'tenant'};
+    die('No OpenStack username provided in "username"')  unless defined $args{'username'};
+    die('No OpenSTack password provided in "password"')  unless defined $args{'password'};
+
+    return $self->{'token'} if defined $self->{'token'};
+
+    my $auth = $self->{'auth'};
+
+    my $response = $auth->call('POST' => '/tokens', {
+        'auth' => {
+            'tenantName'          => $args{'tenant'},
+            'passwordCredentials' => {
+                'username' => $args{'username'},
+                'password' => $args{'password'}
+            }
+        }
+    });
+
+    my $access = $response->{'access'};
+
+    die('No token found in response') unless defined $access->{'token'}->{'id'};
+
+    #
+    # Create a new client object for each endpoint listed in the service
+    # catalog, and store the token alongside
+    #
+    foreach my $service (@{$access->{'serviceCatalog'}}) {
+        my $name = $service->{'name'};
+
+        $self->{'endpoints'}->{$name} = $service->{'endpoints'};
+    }
+
+    return $self->{'token'} = $access->{'token'};
+}
+
+=item C<$client-E<gt>token()>
+
+Return an authorization token obtained from the last successful Keystone
+authentication.
+
+=cut
+
+sub token ($) {
+    my ($self) = @_;
+
+    die('Not authenticated') unless defined $self->{'token'};
+
+    return $self->{'token'};
+}
+
+=back
+
+=head1 CONNECTING TO OPENSTACK SERVICES
+
+=over
+
+=item C<$client-E<gt>services()>
+
+Return a list of service names C<$client> is authorized to access.
+
+=cut
+
+sub services ($) {
+    return sort keys %{shift->{'endpoints'}};
+}
+
+=item C<$client-E<gt>service(I<$name>, I<%opts>)>
+
+Obtain a client to the OpenStack service I<$name>.  The following values may be
+specified in I<%opts> to help locate the most appropriate endpoint for a given
+service:
+
+=over
+
+=item * B<region>
+
+When specified, attempt to obtain a client for the endpoint for that region.
+When not specified, the a client for the first endpoint found for service
+I<$name> is returned instead.
+
+=item * B<public>
+
+When specified (and set to 1), a client is opened for the public endpoint
+corresponding to service I<$name>.
+
+=item * B<internal>
+
+When specified (and set to 1), a client is opened for the internal endpoint
+corresponding to service I<$name>.
+
+=item * B<admin>
+
+When specified (and set to 1), a client is opened for the administrative
+endpoint corresponding to service I<$name>.
+
+=back
+
+=cut
 
 sub service ($$%) {
     my ($self, $name, %opts) = @_;
@@ -67,54 +224,8 @@ sub service ($$%) {
     die("Could not find endpoint '$name'");
 }
 
-sub services ($) {
-    return sort keys %{shift->{'endpoints'}};
-}
+=back
 
-sub token ($) {
-    my ($self) = @_;
-
-    die('Not authenticated') unless defined $self->{'token'};
-
-    return $self->{'token'};
-}
-
-sub tenant ($) {
-    shift->token->{'tenant'};
-}
-
-sub auth ($$$) {
-    my ($self, %opts) = @_;
-
-    return $self->{'token'} if defined $self->{'token'};
-
-    my $auth = $self->{'auth'};
-
-    my $response = $auth->call('POST' => '/tokens', {
-        'auth' => {
-            'tenantName'          => $opts{'tenant'},
-            'passwordCredentials' => {
-                'username' => $opts{'username'},
-                'password' => $opts{'password'}
-            }
-        }
-    });
-
-    my $access = $response->{'access'};
-
-    die('No token found in response') unless defined $access->{'token'}->{'id'};
-
-    #
-    # Create a new client object for each endpoint listed in the service
-    # catalog, and store the token alongside
-    #
-    foreach my $service (@{$access->{'serviceCatalog'}}) {
-        my $name = $service->{'name'};
-
-        $self->{'endpoints'}->{$name} = $service->{'endpoints'};
-    }
-
-    return $self->{'token'} = $access->{'token'};
-}
+=cut
 
 1;
